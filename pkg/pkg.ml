24e0e3275ff58217a822a6ea78ef36416ca74d21
Anil Madhavapeddy <anil@recoil.org>
2017-05-23 22:25:26 +0100

    port to jbuilder and activate travis ci

--- pkg/pkg.ml
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ocaml
-#use "topfind"
-#require "topkg"
-open Topkg
-
-let () =
-  Pkg.describe "mirage-device" @@ fun c ->
-  Ok [ Pkg.lib "pkg/META";
-       Pkg.mllib "src//mirage-device.mllib" ]
